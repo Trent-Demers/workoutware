298484180ff37714b996a3107874131e005b585f
Mahek Patel <120302903+Mkpz@users.noreply.github.com>
2025-12-05 10:06:37 -0500

    Renamed metric_id to progress_id

--- sql/workoutware_db_setup.sql
+++ sql/workoutware_db_setup.sql
@@ -353,7 +353,7 @@ DROP TABLE IF EXISTS `progress`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `progress` (
-  `metric_id` int NOT NULL AUTO_INCREMENT,
+  `progress_id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `exercise_id` int NOT NULL,
   `date` date NOT NULL,
@@ -362,7 +362,7 @@ CREATE TABLE `progress` (
   `avg_weight` decimal(6,2) DEFAULT NULL,
   `total_volume` decimal(10,2) DEFAULT NULL,
   `workout_count` int DEFAULT NULL,
-  PRIMARY KEY (`metric_id`),
+  PRIMARY KEY (`progress_id`),
   KEY `user_id` (`user_id`),
   KEY `exercise_id` (`exercise_id`),
   CONSTRAINT `progress_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE,
